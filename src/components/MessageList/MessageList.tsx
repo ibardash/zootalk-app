073f52fa6cf822f52b992bfd311aa59a6961e936
iana.bardash <iana.bardash@ladbrokes.com.au>
2022-01-09 23:35:32 +1100

    feat: scroll to the bottom of the message list when the list changes

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,20 +1,35 @@
 import { MessageBubble } from "../MessageBubble";
 import styled from "styled-components";
+import { useCallback, useEffect, useRef } from "react";
 
 export interface MessageListProps {
   messages: string[];
 }
 
+const SCROLL_DELAY_MS = 300;
+
 export function MessageList({ messages }: MessageListProps) {
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollToBottom = useCallback(() => {
+    messagesEndRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => scrollToBottom(), SCROLL_DELAY_MS);
+  }, [messages, scrollToBottom]);
+
   return (
     <OuterContainer>
       {messages.map((message, i) => (
         <MessageBubble key={i}>{message}</MessageBubble>
       ))}
+      <div ref={messagesEndRef} />
     </OuterContainer>
   );
 }
 
 const OuterContainer = styled.div`
   overflow-y: scroll;
+  flex-grow: 1;
 `;
