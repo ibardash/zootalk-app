5083aab30003f6946736eef459e2d7fb071025a1
iana.bardash <iana.bardash@ladbrokes.com.au>
2022-01-11 09:51:54 +1100

    feat: programmatically navigate

--- src/WelcomeScreen.tsx
+++ src/WelcomeScreen.tsx
@@ -1,5 +1,8 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function WelcomeScreen() {
-  return <Link to="/chat">Chat now</Link>;
+  const navigate = useNavigate();
+  const handleClick = () => navigate("/chat");
+
+  return <button onClick={handleClick}>Chat now</button>;
 }
